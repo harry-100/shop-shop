b728e6d5de740facaa553e42c38dddb94e562647
Harry Shah <harvinder.shah@gmail.com>
2021-11-08 12:25:15 -0500

    global state added

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,4 @@
+import { useReducer } from "react";
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
@@ -6,14 +7,29 @@ import {
 
 export const reducer = (state, action) => {
     switch (action.type) {
-         case UPDATE_PRODUCTS:
-             return {
-                 ...state,
-                 products: [...action.products],
-             };
-
-            //  if its none of these actions, do not update state at all and keep things the same.
-            default:
-                return state;
+      
+      // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+      case UPDATE_CATEGORIES:
+        return {
+          ...state,
+          categories: [...action.categories]
+        };
+      // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+      case UPDATE_PRODUCTS:
+        return {
+          ...state,
+          products: [...action.products]
+        }; 
+    
+        case UPDATE_CURRENT_CATEGORY:
+            return{
+                ...state,
+                currentCategory: action.currentCategory
+            };
+      default:
+        return state;
     }
-};
+  };
+  export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+  }
